f62baaa1e960600ab933f035805adddca10406ae
DESKTOP-7CLU8AB\dev3_4 <uhas2003@naver.com>
2022-03-11 17:59:36 +0900

    a

--- laravelapi/routes/api.php
+++ laravelapi/routes/api.php
@@ -2,17 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+
+Route::middleware(['auth:sanctum', 'isAPIAdmin'])->group(function () {
+
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::get('/checkingAuthenticated', function() {
+        return response()->json(['messgae'=>'You are in', 'status'=>200], 200);
+    });
+
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
